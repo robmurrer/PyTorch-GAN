4151cae8b5fc353af8a4df50bb4d57746dd151c3
statcheck-robmurrer <rob@statcheck.us>
2024-09-24 09:54:23 -0500

    some small changes to develop a bench and plots

--- implementations/wgan/wgan.py
+++ implementations/wgan/wgan.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import math
 import sys
+from datetime import datetime
 
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
@@ -15,6 +16,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+from matplotlib import pyplot as plt
+
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
@@ -22,7 +25,7 @@ parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs 
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.00005, help="learning rate")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--latent_dim", type=int, default=400, help="dimensionality of the latent space")
 parser.add_argument("--img_size", type=int, default=28, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--n_critic", type=int, default=5, help="number of training steps for discriminator per iter")
@@ -107,61 +110,70 @@ optimizer_D = torch.optim.RMSprop(discriminator.parameters(), lr=opt.lr)
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
-# ----------
-#  Training
-# ----------
+g_losses = []
+d_losses = []
+
+def plot_losses(g_losses, d_losses, save_path):
+    plt.figure(figsize=(10, 5))
+    plt.title("Generator and Discriminator Loss During Training")
+    plt.plot(g_losses, label="G")
+    plt.plot(d_losses, label="D")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig(save_path)
+    plt.close()
 
 batches_done = 0
 for epoch in range(opt.n_epochs):
-
     for i, (imgs, _) in enumerate(dataloader):
-
         # Configure input
         real_imgs = Variable(imgs.type(Tensor))
-
         # ---------------------
         #  Train Discriminator
         # ---------------------
-
         optimizer_D.zero_grad()
-
         # Sample noise as generator input
         z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
-
         # Generate a batch of images
         fake_imgs = generator(z).detach()
         # Adversarial loss
         loss_D = -torch.mean(discriminator(real_imgs)) + torch.mean(discriminator(fake_imgs))
-
         loss_D.backward()
         optimizer_D.step()
-
         # Clip weights of discriminator
         for p in discriminator.parameters():
             p.data.clamp_(-opt.clip_value, opt.clip_value)
-
         # Train the generator every n_critic iterations
         if i % opt.n_critic == 0:
-
             # -----------------
             #  Train Generator
             # -----------------
-
             optimizer_G.zero_grad()
-
             # Generate a batch of images
             gen_imgs = generator(z)
             # Adversarial loss
             loss_G = -torch.mean(discriminator(gen_imgs))
-
             loss_G.backward()
             optimizer_G.step()
-
             print(
                 "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
                 % (epoch, opt.n_epochs, batches_done % len(dataloader), len(dataloader), loss_D.item(), loss_G.item())
             )
+            
+            # Save losses for plotting
+            g_losses.append(loss_G.item())
+            d_losses.append(loss_D.item())
 
         if batches_done % opt.sample_interval == 0:
             save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
         batches_done += 1
+
+    # Plot losses after each epoch
+    if (epoch + 1) % 10 == 0:  # Plot every 10 epochs, adjust as needed
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        plot_losses(g_losses, d_losses, f"loss_plot_{timestamp}.png")
+
+# Final plot after all epochs
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+plot_losses(g_losses, d_losses, f"final_loss_plot_{timestamp}.png")
